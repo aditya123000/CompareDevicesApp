52ede49d46f4e0b402464cc1af80458aeb5dfc36
aditya123000 <aditdwivedi336@gmail.com>
2026-02-03 23:14:26 +0530

    Setup Tailwind Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>App Loaded</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">
+      Tailwind is working
+    </h1>
+  );
 }
 
 export default App;
